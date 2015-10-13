5b99e212d26ade1a5f07b80ca760081ecccacd39
jabhishek <abhi2000@gmail.com>
2015-10-13 16:08:39 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+/* eslint no-console: 0 */
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config.js';
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const defaultPort = 8000;
+const port = isDeveloping ? defaultPort : (process.env.PORT || defaultPort);
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+if (isDeveloping) {
+  const compiler = webpack(config);
+
+  app.use(webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  }));
+
+  app.use(webpackHotMiddleware(compiler));
+}
+
+app.get('*', function response(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, 'localhost', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+});
