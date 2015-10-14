37b64735ed8ea7d438cf26012910a12af3fa5c49
jabhishek <abhi2000@gmail.com>
2015-10-14 11:45:10 +0100

    Tidying up; removing unused modules

--- client/components/home/homePage.js
+++ client/components/home/homePage.js
@@ -3,12 +3,13 @@ import React from 'react';
 export default class HomePage extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = { param: 'Hello'};
 	}
 
 	render() {
 		return (
 			<div className="home-page">
-				<h1>Home</h1>
+				<h1>{this.state.param + ' World!'}</h1>
 			</div>
 		);
 	}
--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ import path from 'path';
 import express from 'express';
 import webpack from 'webpack';
 import webpackDevServer from 'webpack-dev-server';
-import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './../webpack.config.js';
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
@@ -16,27 +15,22 @@ app.use(express.static(rootPath + '/dist'));
 if (isDeveloping) {
 	const compiler = webpack(config);
 
-	var server = new webpackDevServer(webpack(config), {
+	var server = new webpackDevServer(compiler, {
 		publicPath: config.output.publicPath,
 		historyApiFallback: true,
 		hot: true
 	});
 
-	//server.use(webpackHotMiddleware(compiler));
-
 	server.listen(8080);
-
-
 } else {
 	app.get('*', function response(req, res) {
 		res.sendFile(path.join(rootPath, 'dist/index.html'));
 	});
 
-	app.listen(port, 'localhost', function onStart(err) {
+	app.listen(port, function onStart(err) {
 		if (err) {
 			console.log(err);
 		}
-		console.info('==> ? Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
 	});
 }
 
